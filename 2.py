b3a25ae6a1547d2c621fffbe248aeaf63696ec97
uldiniumGit <114669125+uldiniumGit@users.noreply.github.com>
2025-06-01 17:32:46 +0700

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,19 +1,15 @@
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String, create_engine, and_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///orm.sqlite', echo=False)
-
 Base = declarative_base()
-
 
 class Region(Base):
     __tablename__ = 'region'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     number = Column(Integer, nullable=True)
-
-    # note = Column(String, nullable=True)
 
     def __init__(self, name, number):
         self.name = name
@@ -22,59 +18,16 @@ class Region(Base):
     def __str__(self):
         return f'{self.id}) {self.name}: {self.number}'
 
-
-# Создание таблицы
 Base.metadata.create_all(engine)
-
-# Создание сессии
-# create a configured "Session" class
 Session = sessionmaker(bind=engine)
-
-# create a Session
 session = Session()
 
-# region = Region('Москва', 1)
-#
-# # Добавление данных
-# session.add(region)
-#
-# region = Region('Питер', 78)
-#
-# # Добавление данных
-# session.add(region)
-#
-# session.commit()
-#
-#
-# # Изменение данных
-# region.name = 'Тула'
-#
-# session.commit()
-#
-# # Удаление данных
-# session.delete(region)
-# session.commit()
-#
-# for i in range(10):
-#     region = Region(f'region {i}', i)
-#     session.add(region)
-#
-# session.commit()
-#
+# Пример выборки с условием
+regions = session.query(Region).filter(
+    and_(Region.name == 'Москва', Region.id > 0)
+).all()
 
-# Выборка данных
-# 1. Все регионы, которые есть в базе
-regions = session.query(Region).all()
-# Класс запроса
 print(type(regions))
-
 for region in regions:
     print(region)
 
-
-# Запрос с условием
-regions = session.query(Region).filter(Region.name == 'Москва' and Region.id > 0).all()
-print(type(regions))
-
-for region in regions:
-    print(region)
